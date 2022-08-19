5cd2b37dc77435f78c59c387f49e2cba7e62aa48
Arini11 <elarnaumc@gmail.com>
2022-08-19 10:24:11 +0200

    setup

---
+++ src/test/java/team5/proyecto/reservesMenjador/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.proyecto.reservesMenjador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
