452e4ded2d25efe05a15625f630891dabe6225a9
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-19 13:15:51 +0200

    Update usersServiceImpl.java

--- src/main/java/team5/proyecto/reservesMenjador/services/usersServiceImpl.java
+++ src/main/java/team5/proyecto/reservesMenjador/services/usersServiceImpl.java
@@ -1,5 +1,46 @@
 package team5.proyecto.reservesMenjador.services;
 
-public class usersServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import team5.proyecto.reservesMenjador.dao.IUsersDAO;
+import team5.proyecto.reservesMenjador.dto.Users;
+
+
+@Service
+public class usersServiceImpl implements IusersService {
+	
+	@Autowired
+	IUsersDAO iusersDAO;
+
+	@Override
+	public List<Users> listarUsers() {
+		return iusersDAO.findAll();
+	}
+
+	@Override
+	public Users guardarUser(Users users) {
+		return iusersDAO.save(users);
+	}
+
+	@Override
+	public Users usersPorId(Long Id) {
+	
+		return iusersDAO.findById(Id).get();
+	}
+
+	@Override
+	public Users actualizarUser(Users users) {
+		
+		return iusersDAO.save(users);
+	}
+
+	@Override
+	public void eliminarUser(Long Id) {
+		iusersDAO.deleteById(Id);
+	} 
+	
+	
 
 }
