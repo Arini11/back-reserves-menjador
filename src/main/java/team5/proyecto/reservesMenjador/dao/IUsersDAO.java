397acfb80f969d0ccd674f63fb7ec3e22720c4e3
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-19 13:15:29 +0200

    Create IUsersDAO.java

---
+++ src/main/java/team5/proyecto/reservesMenjador/dao/IUsersDAO.java
@@ -0,0 +1,8 @@
+package team5.proyecto.reservesMenjador.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import team5.proyecto.reservesMenjador.dto.Users;
+
+public interface IUsersDAO extends JpaRepository<Users, Long> {
+}
