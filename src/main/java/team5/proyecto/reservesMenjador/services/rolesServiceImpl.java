551209214ac671ce91ad614d08a7863475998aba
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-19 13:15:47 +0200

    Update rolesServiceImpl.java

--- src/main/java/team5/proyecto/reservesMenjador/services/rolesServiceImpl.java
+++ src/main/java/team5/proyecto/reservesMenjador/services/rolesServiceImpl.java
@@ -1,5 +1,46 @@
 package team5.proyecto.reservesMenjador.services;
 
-public class rolesServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import team5.proyecto.reservesMenjador.dao.IRolesDAO;
+import team5.proyecto.reservesMenjador.dto.Roles;
+
+@Service
+public class rolesServiceImpl implements IrolesService{
+
+	@Autowired
+	IRolesDAO irolesDAO;
+
+	@Override
+	public List<Roles> listarRoles() {
+		
+		return irolesDAO.findAll();
+	}
+
+	@Override
+	public Roles guardarRol(Roles roles) {
+		return irolesDAO.save(roles);
+	}
+
+	@Override
+	public Roles rolesPorId(Long Id) {
+		
+		return irolesDAO.findById(Id).get();
+	}
+
+	@Override
+	public Roles actualizarRol(Roles roles) {
+		
+		return irolesDAO.save(roles);
+	}
+
+	@Override
+	public void eliminarRol(Long Id) {
+		irolesDAO.deleteById(Id);
+		
+	}
+	
+	
 }
