e0e83b6449671a94c8f09219e5eb3e2aa50bdb5a
Joana <joanajanefdez@gmail.com>
2022-08-24 17:52:35 +0200

    put methods

--- src/main/java/team5/proyecto/reservesMenjador/services/ICategoryService.java
+++ src/main/java/team5/proyecto/reservesMenjador/services/ICategoryService.java
@@ -7,15 +7,15 @@ import team5.proyecto.reservesMenjador.dto.Dish;
 
 public interface ICategoryService {
 	//CRUD	
-	public List<Category> getCategories();
-
-	public Category saveCategory(Category categ); //CREATE || UPDATE
+	public List<Category> getCategories();	
 
 	public Category findById(int id);
 	
 	public Category findByName(String name);
 	
 	public List<Category> findByDishes(Dish dish);
+	
+	public Category saveCategory(Category categ); //CREATE || UPDATE
 
 	public void deleteCategory(int id);	
 
